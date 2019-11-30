93e712f89d6918688b849f6d49fc3cf789145cc8
Pine Mizune <pinemz@gmail.com>
2019-11-30 17:30:31 +0900

    test: add tests

--- src/test/java/moe/pine/reactor/interruptedexception/ReactiveExceptionsTest.java
+++ src/test/java/moe/pine/reactor/interruptedexception/ReactiveExceptionsTest.java
@@ -1,4 +1,56 @@
 package moe.pine.reactor.interruptedexception;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.Exceptions;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ReactiveExceptionsTest {
+    @Test
+    public void isInterruptedTest_interruptedException() {
+        assertTrue(ReactiveExceptions.isInterrupted(new InterruptedException()));
+    }
+
+    @Test
+    public void isInterruptedTest_IOException() {
+        assertFalse(ReactiveExceptions.isInterrupted(new IOException()));
+    }
+
+    @Test
+    public void isInterruptedTest_runtimeException() {
+        assertFalse(ReactiveExceptions.isInterrupted(new RuntimeException()));
+    }
+
+    @Test
+    public void isInterruptedTest_wrapped_interruptedException() {
+        final RuntimeException e = Exceptions.propagate(new InterruptedException());
+        assertTrue(ReactiveExceptions.isInterrupted(e));
+    }
+
+    @Test
+    public void isInterruptedTest_wrapped_IOException() {
+        final RuntimeException e = Exceptions.propagate(new IOException());
+        assertFalse(ReactiveExceptions.isInterrupted(e));
+    }
+
+    @Test
+    public void isInterruptedTest_multiple_interruptedException() {
+        final RuntimeException e = Exceptions.multiple(
+            new RuntimeException(),
+            new InterruptedException()
+        );
+        assertTrue(ReactiveExceptions.isInterrupted(e));
+    }
+
+    @Test
+    public void isInterruptedTest_multiple_IOExceptions() {
+        final RuntimeException e = Exceptions.multiple(
+            new RuntimeException(),
+            new IOException()
+        );
+        assertFalse(ReactiveExceptions.isInterrupted(e));
+    }
 }
