84a7474f4913654abf7a4d4e0e671512c07b2852
Pine Mizune <pinemz@gmail.com>
2021-06-28 14:33:20 +0900

    test: use @Nested

--- src/test/java/moe/pine/reactor/interruptible/ReactiveExceptionsTest.java
+++ src/test/java/moe/pine/reactor/interruptible/ReactiveExceptionsTest.java
@@ -1,5 +1,6 @@
 package moe.pine.reactor.interruptible;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import reactor.core.Exceptions;
 
@@ -12,79 +13,86 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
+@SuppressWarnings("ALL")
 class ReactiveExceptionsTest {
-    @Test
-    void isInterruptedTest_interruptedException() {
-        assertTrue(ReactiveExceptions.isInterrupted(new InterruptedException()));
-    }
+    @Nested
+    class IsInterrupted {
+        @Test
+        void interruptedException() {
+            assertTrue(ReactiveExceptions.isInterrupted(new InterruptedException()));
+        }
 
-    @Test
-    void isInterruptedTest_IOException() {
-        assertFalse(ReactiveExceptions.isInterrupted(new IOException()));
-    }
+        @Test
+        void ioException() {
+            assertFalse(ReactiveExceptions.isInterrupted(new IOException()));
+        }
 
-    @Test
-    void isInterruptedTest_runtimeException() {
-        assertFalse(ReactiveExceptions.isInterrupted(new RuntimeException()));
-    }
+        @Test
+        void runtimeException() {
+            assertFalse(ReactiveExceptions.isInterrupted(new RuntimeException()));
+        }
 
-    @Test
-    void isInterruptedTest_wrapped_interruptedException() {
-        RuntimeException e = Exceptions.propagate(new InterruptedException());
-        assertTrue(ReactiveExceptions.isInterrupted(e));
-    }
+        @Test
+        void wrapped_interruptedException() {
+            RuntimeException e = Exceptions.propagate(new InterruptedException());
+            assertTrue(ReactiveExceptions.isInterrupted(e));
+        }
 
-    @Test
-    void isInterruptedTest_wrapped_IOException() {
-        RuntimeException e = Exceptions.propagate(new IOException());
-        assertFalse(ReactiveExceptions.isInterrupted(e));
-    }
+        @Test
+        void wrapped_IOException() {
+            RuntimeException e = Exceptions.propagate(new IOException());
+            assertFalse(ReactiveExceptions.isInterrupted(e));
+        }
 
-    @Test
-    void isInterruptedTest_multiple_interruptedException() {
-        RuntimeException e =
-                Exceptions.multiple(
-                        new RuntimeException(),
-                        new InterruptedException()
-                );
-        assertTrue(ReactiveExceptions.isInterrupted(e));
-    }
+        @Test
+        void multiple_interruptedException() {
+            RuntimeException e =
+                    Exceptions.multiple(
+                            new RuntimeException(),
+                            new InterruptedException()
+                    );
+            assertTrue(ReactiveExceptions.isInterrupted(e));
+        }
 
-    @Test
-    void isInterruptedTest_multiple_IOExceptions() {
-        RuntimeException e =
-                Exceptions.multiple(
-                        new RuntimeException(),
-                        new IOException()
-                );
-        assertFalse(ReactiveExceptions.isInterrupted(e));
-    }
-
-    @Test
-    void unwrapInterruptedTest_runtimeException() throws InterruptedException {
-        RuntimeException e1 = new RuntimeException();
-        try {
-            ReactiveExceptions.unwrapInterrupted(() -> {
-                throw e1;
-            });
-            fail();
-        } catch (RuntimeException e2) {
-            assertSame(e1, e2);
+        @Test
+        void multiple_IOExceptions() {
+            RuntimeException e =
+                    Exceptions.multiple(
+                            new RuntimeException(),
+                            new IOException()
+                    );
+            assertFalse(ReactiveExceptions.isInterrupted(e));
         }
     }
 
-    @Test
-    void unwrapInterruptedTest_wrapped_interruptedException() throws InterruptedException {
-        InterruptedException e1 = new InterruptedException();
-        try {
-            ReactiveExceptions.unwrapInterrupted(() -> {
-                throw Exceptions.propagate(e1);
-            });
-            fail();
-        } catch (InterruptedException e2) {
-            assertNotSame(e1, e2);
-            assertTrue(e2.getCause() instanceof RuntimeException);
-            assertSame(e1, e2.getCause().getCause());
+    @Nested
+    class UnwrapInterrupted {
+        @Test
+        void runtimeException() throws InterruptedException {
+            RuntimeException e1 = new RuntimeException();
+            try {
+                ReactiveExceptions.unwrapInterrupted(() -> {
+                    throw e1;
+                });
+                fail();
+            } catch (RuntimeException e2) {
+                assertSame(e1, e2);
+            }
+        }
+
+        @Test
+        void wrapped_interruptedException() throws InterruptedException {
+            InterruptedException e1 = new InterruptedException();
+            try {
+                ReactiveExceptions.unwrapInterrupted(() -> {
+                    throw Exceptions.propagate(e1);
+                });
+                fail();
+            } catch (InterruptedException e2) {
+                assertNotSame(e1, e2);
+                assertTrue(e2.getCause() instanceof RuntimeException);
+                assertSame(e1, e2.getCause().getCause());
+            }
         }
     }
 }
