5376d612d4ed651a6e289a530d1bfc910b7eb416
Pine Mizune <pinemz@gmail.com>
2021-08-16 18:06:41 +0900

    fix: remove wrong @Nullable annocation

--- src/main/java/moe/pine/reactor/interruptible/MonoUtils.java
+++ src/main/java/moe/pine/reactor/interruptible/MonoUtils.java
@@ -4,26 +4,27 @@ import moe.pine.reactor.interruptible.annotation.Nullable;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.util.Objects;
 import java.util.Optional;
+
+import static moe.pine.reactor.interruptible.ReactiveExceptions.unwrapInterrupted;
 
 public abstract class MonoUtils {
     @Nullable
     public static <T> T block(Mono<T> mono) throws InterruptedException {
-        return ReactiveExceptions.unwrapInterrupted(mono::block);
+        return unwrapInterrupted(mono::block);
     }
 
     @Nullable
     public static <T> T block(Mono<T> mono, Duration timeout) throws InterruptedException {
-        return ReactiveExceptions.unwrapInterrupted(() -> mono.block(timeout));
+        return unwrapInterrupted(() -> mono.block(timeout));
     }
 
-    @Nullable
     public static <T> Optional<T> blockOptional(Mono<T> mono) throws InterruptedException {
-        return ReactiveExceptions.unwrapInterrupted(mono::blockOptional);
+        return Objects.requireNonNull(unwrapInterrupted(mono::blockOptional));
     }
 
-    @Nullable
     public static <T> Optional<T> blockOptional(Mono<T> mono, Duration timeout) throws InterruptedException {
-        return ReactiveExceptions.unwrapInterrupted(() -> mono.blockOptional(timeout));
+        return Objects.requireNonNull(unwrapInterrupted(() -> mono.blockOptional(timeout)));
     }
 }
